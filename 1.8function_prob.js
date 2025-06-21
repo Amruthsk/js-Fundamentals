1d208b84c48420416a464965ffb3ba83086a5a48
Amruth S K <amruthsk25@gmail.com>
2025-06-21 21:20:53 +0530

    pattern problems

--- 1.8function_prob.js
+++ 1.8function_prob.js
@@ -22,21 +22,104 @@ console.log(ans);
 
 //Pattern problem -1
 
-// Problem - 1 Given a value n - print the following pattern
+// problem  0 print n stars in a line
+// approach
+// loop to repeate task for n rows
+// Loop column repeate task for n columns  concatenate string of * stars n times 
+function oneLine(n){
+   for(let row = 1; row <=1; row++){
+    //concatenate
+    let star = " ";
+    for (let col = 1; col <= n; col++) {
+      star += "*";
+    }
+    console.log(star);
+   }
+
+      
+}
+
+let star = oneLine(5);
+console.log(star);
+
+// Problem - 1 Given a value n - print the Square pattern
 
 // approach
 //i/p - 2 op- 2 X 2 stars
 //i/p - 4 op- 4 X 4 stars
-//Loop row
+//Loop row repeat task for n rows
+//Loop column repeate task for n columns & concatenate
+
 //Loop column
 
-function printStar(n){
+function printSquareStar(n){
+  //loop
     for (let row = 1; row <= n; row++) {
+      //concatenate
+        let star =" ";
       for (let j = 1; j <= n; j++) {
-        console.log("*");
+         star += "*";
       }
+      console.log(star)
+
     }
 }
+//return - undefined
+let Squarestar = printSquareStar(3);
+console.log(Squarestar)
 
-let star = printStar(2);
-console.log(star)
+
+//Triangle pattern
+
+// Problem - 1 Given a value n - print the Triangle pattern
+
+// approach
+//i/p - 2 op- 1,2  stars
+//i/p - 4 op- 1,2,3,4 stars
+//Loop row to repeate task for n rows
+//Loop column repeate task for n columns- condition< = row & concatenate
+
+function righttrianglepattern(n) {
+  //loop
+  for (let row = 1; row <= n; row++) {
+    //concatenate
+    let star = " ";
+    for (let j = 1; j <= row; j++) {
+      star += "*";
+    }
+    console.log(star);
+  }
+}
+// return- undefined
+let Trianglestar = righttrianglepattern(3);
+console.log(Trianglestar);
+
+
+
+//mirror Triangle pattern
+
+// Problem - 1 Given a value n - print the mirror Triangle pattern
+
+// approach
+//i/p - 2 op- 1,2  stars - n-1 space
+//i/p - 4 op- 1,2,3,4 stars- 
+//Loop row to repeate task for n rows - for ith row 1)print n-1 space 2) print i star
+//Loop column repeate task for n columns- condition< = row & concatenate
+
+function invertedtrianglepattern(n) {
+  //loop
+  for (let row = 1; row <= n; row++) {
+    //concatenate
+    let spacestar = " ";
+    for (let space = 1; space <= n-row; space++){
+      spacestar += " ";
+
+    }for (let col = 1; col <= row; col++) {
+      spacestar += "*";
+    }
+    console.log(spacestar);
+  }
+}
+// return- undefined
+let InvertedTrianglestar = invertedtrianglepattern(4);
+console.log(InvertedTrianglestar);
