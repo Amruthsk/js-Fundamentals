b5f5c1a85c7bc3c558d1639febe10d0f23e14f6e
Amruth S K <amruthsk25@gmail.com>
2025-06-27 18:22:20 +0530

    datatype-function problem-new kw

--- 2 datatypes.js
+++ 2 datatypes.js
@@ -1,9 +1,7 @@
-
 // comments - 2 ways - single line - //, multi-line - /* */- for documentation-better understanding-not executed
 /* this
 is 
-multiline comment*/ 
-
+multiline comment*/
 
 // Data types - how data is stored in memory
 //number
@@ -18,23 +16,23 @@ console.log(-0); // -0 is a number - negative zero value
 console.log(NaN); // NaN is a number - not a number - invalid number
 console.log(Infinity); // Infinity is a number - greater than maximum limit of number type
 console.log(-Infinity); // -Infinity is a number - less than minimum limit of number type
-console.log(1/0); // Infinity - division by zero
-console.log(1/-0); // -Infinity - division by negative zero
-console.log(0/0); // NaN - division of zero by zero
-console.log(10 < Infinity)// Infinity - division by zero
+console.log(1 / 0); // Infinity - division by zero
+console.log(1 / -0); // -Infinity - division by negative zero
+console.log(0 / 0); // NaN - division of zero by zero
+console.log(10 < Infinity); // Infinity - division by zero
 
 //where 0 - zero - used for zero value
 //where -0 - negative zero - used for negative zero value - direction ,velocity, etc.
 //where NaN - not a number - used for invalid numbers - not  present in memory - bound to return a number but no t a valid number
 //where Infinity - used for numbers greater than the maximum limit of number type
 //where -Infinity - used for numbers less than the minimum limit of number type
-console.log(10/null); // NaN
-console.log(10/0); // Infinity
-console.log(10/-0); // -Infinity
-console.log(10/undefined); // NaN
-console.log(undefined/10); // NaN
-console.log(undefined/undefined); // NaN
-console.log(undefined/null); // NaN
+console.log(10 / null); // NaN
+console.log(10 / 0); // Infinity
+console.log(10 / -0); // -Infinity
+console.log(10 / undefined); // NaN
+console.log(undefined / 10); // NaN
+console.log(undefined / undefined); // NaN
+console.log(undefined / null); // NaN
 
 // whichi is the only number that is not equal to itself?
 console.log(NaN === NaN); // false - NaN is not equal to itself
@@ -49,7 +47,7 @@ const text3 = "javascript is a programming language \b easy to learn";
 const text4 = "javascript is a programming language \f easy to learn";
 const text5 = "javascript is a programming language \v easy to learn";
 console.log(text);
-console.log(text1); 
+console.log(text1);
 console.log(text2);
 console.log(text3);
 console.log(text4);
@@ -57,7 +55,7 @@ console.log(text5);
 
 //boolean
 //undefined - may be defined later
-//objects - key-value pair - combination of above 
+//objects - key-value pair - combination of above
 
 // 2 types
 //Primitive - exist themselves
@@ -80,9 +78,8 @@ b = null;
 
 let a;
 
-
 var marks = 100;
-var name = "Coding is easy"
+var name = "Coding is easy";
 var age;
 var company = null;
 var salary = undefined;
@@ -94,6 +91,3 @@ console.log("age = ", age);
 console.log("company = ", company);
 console.log("salary = ", salary);
 console.log("isStudent = ", isStudent);
-
-
-
--- 3.1.1 function_prob.js
+++ 3.1.1 function_prob.js
@@ -1,51 +1,49 @@
 //problem - Given a number write a function to determine whether the no. is prime or not
- 
-//approach 
 
-//input - 2 output -prime 
+//approach
+
+//input - 2 output -prime
 //input - 4 not prime
 
-// check if divisible by any no less than given number not prime /prime 
+// check if divisible by any no less than given number not prime /prime
 //Loop
 
-function isPrime(n){
-    for(let i = 2; i < n; i++){
-        if(n % i == 0){
-            return "Not Prime"
-        }
+function isPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return "Not Prime";
     }
-    return "Prime"
+  }
+  return "Prime";
 }
 
 let ans = isPrime(11);
 console.log(ans);
 
-console.log("pattern1")
+console.log("Star pattern");
+console.log("pattern1");
 //Pattern problem -1
 // ****
 // problem  0 print n stars in a line
 // approach
 // loop to repeate task for n rows- for each row
-// inside row do Loop column repeate task for n columns  concatenate string of * stars n times 
-function oneLine(n){
-  //loop each row 
+// inside row do Loop column repeate task for n columns  concatenate string of * stars n times
+function oneLine(n) {
+  //loop each row
   // n=4,row = single row =1 ->  ****
-   for(let row = 1; row <=1; row++){
-     //loop to concatenate n star in each row
-     let star = " ";
-     for (let col = 1; col <= n; col++) {
-       star += "*";
-     }
-     //after loop  we will have ****
-     console.log(star);
-   }
-
-      
+  for (let row = 1; row <= 1; row++) {
+    //loop to concatenate n star in each row
+    let star = " ";
+    for (let col = 1; col <= n; col++) {
+      star += "*";
+    }
+    //after loop  we will have ****
+    console.log(star);
+  }
 }
 
 let star = oneLine(4);
 console.log(star);
-
 
 console.log("pattern2");
 // Problem - 2 Given a value n - print the Square pattern
@@ -60,21 +58,21 @@ console.log("pattern2");
 
 //Loop column
 
-function printSquareStar(n){
-  //loop each row n=3 row =3 
-    for (let row = 1; row <= n; row++) {
-      //inside row loop to concatenate star
-      let star = " ";
-      for (let j = 1; j <= n; j++) {
-        star += "*";
-      }
-      //after loop  we will have ***
-      console.log(star);
+function printSquareStar(n) {
+  //loop each row n=3 row =3
+  for (let row = 1; row <= n; row++) {
+    //inside row loop to concatenate star
+    let star = " ";
+    for (let j = 1; j <= n; j++) {
+      star += "*";
     }
+    //after loop  we will have ***
+    console.log(star);
+  }
 }
 //return - undefined
 let Squarestar = printSquareStar(3);
-console.log(Squarestar)
+console.log(Squarestar);
 
 console.log("pattern3");
 //Triangle pattern
@@ -90,7 +88,7 @@ console.log("pattern3");
 //Loop column repeate task for n columns- condition< = row & concatenate
 
 function righttrianglepattern(n) {
-  //loop row n = row =3 
+  //loop row n = row =3
   for (let row = 1; row <= n; row++) {
     //inside row loop to concatenate star
     let star = " ";
@@ -105,15 +103,14 @@ function righttrianglepattern(n) {
 let Trianglestar = righttrianglepattern(3);
 console.log(Trianglestar);
 
-
 console.log("pattern4");
 //mirror Triangle pattern
 
 // Problem - 4 Given a value n - print the mirror Triangle pattern(space & star)
-  //  *
+//  *
 // approach
 //i/p - 2 op- 1,2  stars - n-1 space
-//i/p - 4 op- 1,2,3,4 stars- 
+//i/p - 4 op- 1,2,3,4 stars-
 //Loop row to repeate task for n rows - for each row for ith row 1)print n-1 space 2) print i star
 //inside row Loop column repeate task for n columns- condition< = row & concatenate
 
@@ -172,7 +169,7 @@ console.log("pattern5");
 //i/p - 4 op- 1,3,5,7 stars- n-1 space
 //Loop row to repeate task for n rows - for each row for ith row 1)print n-1 space 2) print 2*row -1 star
 //inside row Loop column repeate task for n columns- condition< = 2*row -1  & concatenate
-console.log("Triangle pattern")
+console.log("Triangle pattern");
 function trianglepattern(n) {
   //loop row n = row =3
   for (let row = 1; row <= n; row++) {
@@ -182,7 +179,7 @@ function trianglepattern(n) {
       spacestar += " ";
     } // at the end of loop will have 3space  concatenated
     // inside row 2nd loop to concatenate stars star = row
-    for (let col = 1; col <= ((2*row)-1); col++) {
+    for (let col = 1; col <= 2 * row - 1; col++) {
       spacestar += "*";
     } // at the end of loop will have 3space 1star  concatenated
     console.log(spacestar);
@@ -192,7 +189,6 @@ function trianglepattern(n) {
 let Triangle = trianglepattern(5);
 console.log(Triangle);
 
-
 console.log("pattern6");
 //pattern 6 problem- Given a value n - print the  Triangle pattern(space & star) n =5
 // *****
@@ -201,10 +197,10 @@ console.log("pattern6");
 // **
 // *
 
-// 
+//
 // approach
 // loop to repeate task for n rows- for each row
-// inside row do Loop column repeate task for n columns  concatenate string of * stars n-row+1 times 
+// inside row do Loop column repeate task for n columns  concatenate string of * stars n-row+1 times
 function pattern_6(n) {
   //loop each row
   // n=4,row = n row =1
@@ -222,7 +218,6 @@ function pattern_6(n) {
 let pattern6 = pattern_6(5);
 console.log(pattern6);
 
-
 //pattern 7 problem- Given a value n n odd only- print the  Diamond pattern(space & star) n =3
 //   *
 //  ***
@@ -233,26 +228,25 @@ console.log(pattern6);
 //Approach
 //bd 2 parts
 //upper
-//loop for each row 
+//loop for each row
 //inside row loop for n-row space  & loop for (2*row)-1 concatenate space & star
 //lower
 //loop for each row  (n-1)
 //inside row loop for n-1 space  & loop for 2*(n-row)-1 concatenate space & star
 
-console.log("Diamond pattern7")
+console.log("Diamond pattern7");
 
-function upperTriangle(n){
-  for (let row=1; row <= n; row++){
-    let star = " "
-    for(space = 1; space <= n- row; space++){
+function upperTriangle(n) {
+  for (let row = 1; row <= n; row++) {
+    let star = " ";
+    for (space = 1; space <= n - row; space++) {
       star += " ";
     }
-    for(col = 1;col <= (2*row -1); col++){
-      star +="*";
+    for (col = 1; col <= 2 * row - 1; col++) {
+      star += "*";
     }
-    console.log(star)
+    console.log(star);
   }
-
 }
 
 function lowerTriangle(n) {
@@ -263,25 +257,23 @@ function lowerTriangle(n) {
     for (space = 1; space <= row; space++) {
       star += " ";
     }
-    for (col = 1; col <= ((2 * (n-row)) - 1); col++) {
+    for (col = 1; col <= 2 * (n - row) - 1; col++) {
       star += "*";
     }
     console.log(star);
   }
-
 }
 
 //combine the pattern
-function Diamondpattern(n){
+function Diamondpattern(n) {
   upperTriangle(n);
   lowerTriangle(n);
 }
 
-
 let pattern7 = Diamondpattern(5);
 console.log(pattern7);
 
-console.log("hourglass")
+console.log("hourglass");
 //hour glass
 function invertedDiamondpattern(n) {
   lowerTriangle(n);
@@ -291,9 +283,8 @@ function invertedDiamondpattern(n) {
 let pattern8 = invertedDiamondpattern(5);
 console.log(pattern8);
 
-
-console.log("Butterfly")
-//pattern - 9 
+console.log("Butterfly");
+//pattern - 9
 //problem- Given a value n n-odd only- print the  butterfly pattern(space & star) n =3
 
 //Approach
@@ -307,17 +298,17 @@ console.log("Butterfly")
 //loop for each row  (n-1)
 //inside row loop for n-1 space  & loop for 2*(n-row)-1 concatenate space & star
 
-function upper(n){
+function upper(n) {
   for (let row = 1; row <= (n - 1) / 2; row++) {
     let star = " ";
     for (let lstar = 1; lstar <= row; lstar++) {
-      star +="*";
+      star += "*";
     }
-    for (let space = 1; space <= n-(2*row); space++) {
-      star +=" ";
+    for (let space = 1; space <= n - 2 * row; space++) {
+      star += " ";
     }
     for (let rstar = 1; rstar <= row; rstar++) {
-      star +="*";
+      star += "*";
     }
     console.log(star);
   }
@@ -331,26 +322,25 @@ function upper(n){
 //   }
 //   console.log(str);
 //   }
- 
+
 // }
 
 //no need of loop for 1 row
-function middle(n){
-    let str = " ";
-  for(let star = 1; star <= n; star++){
+function middle(n) {
+  let str = " ";
+  for (let star = 1; star <= n; star++) {
     str += "*";
   }
   console.log(str);
- 
 }
 
 function lower(n) {
   for (let row = 1; row <= (n - 1) / 2; row++) {
     let star = " ";
-    for (let lstar = 1; lstar <= ((n - 1) / 2)-row+1; lstar++) {
+    for (let lstar = 1; lstar <= (n - 1) / 2 - row + 1; lstar++) {
       star += "*";
     }
-    for (let space = 1; space <= (2 * row) - 1; space++) {
+    for (let space = 1; space <= 2 * row - 1; space++) {
       star += " ";
     }
     for (let rstar = 1; rstar <= (n - 1) / 2 - row + 1; rstar++) {
@@ -360,8 +350,7 @@ function lower(n) {
   }
 }
 
-
-function valley(n){
+function valley(n) {
   upper(n);
   middle(n);
 }
@@ -375,7 +364,6 @@ function spacetriangledown(n) {
 let pattern10 = spacetriangledown(5);
 console.log(pattern10);
 
-
 function butterfly(n) {
   upper(n);
   middle(n);
@@ -385,7 +373,6 @@ let pattern11 = butterfly(5);
 console.log(pattern11);
 
 function spacetriangleup(n) {
- 
   lower(n);
   middle(n);
 }
@@ -399,10 +386,34 @@ function spacetriangle(n) {
 let pattern13 = spacetriangle(5);
 console.log(pattern13);
 
-
 function spac(n) {
   upper(n);
   lower(n);
 }
-let pattern14 = spac (5);
+let pattern14 = spac(5);
 console.log(pattern14);
+
+console.log("2 Number pattern");
+
+//problem - given a value n print the following pattern
+
+//
+
+//Quadratic problem
+//Given 3 number a,b,c which represent coefficient of quadratic equation
+//ax^2 + bx + c
+//fint the root of quadratic equation
+
+//approach
+//ip-a,b,c op-real roots
+//real roots (-b+srt(b^2 -4ac))/2a, (-b-srt(b^2 -4ac))/2a
+
+function solveroot(a, b, c) {
+  let sqrValue = Math.sqrt(b * b - 4 * a * c);
+  let root1 = (-b + sqrValue) / (2 * a);
+  let root2 = (-b - sqrValue) / (2 * a);
+
+  console.log(root1, root2);
+}
+
+solveroot(2, 5, 3);
